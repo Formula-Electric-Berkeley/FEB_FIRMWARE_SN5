cdcabdfc6b0edccb8851a65cb12041d987a5933f
asymptonic <86031051+asymptonic@users.noreply.github.com>
2026-02-26 21:57:20 -0800

    progress on dash io expander

--- DASH/Core/User/Src/FEB_IO.c
+++ DASH/Core/User/Src/FEB_IO.c
@@ -1,30 +1,48 @@
-// #include "FEB_IO.h"
-// #include "FEB_CAN.h"
-// #include "FEB_i2c_protected.h"
+#include "FEB_IO.h"
+#include "FEB_CAN.h"
+#include "FEB_CAN_PCU.h"
+#include "FEB_i2c_protected.h"
+#include <stdbool.h>
 
 // /* ------------------- External handles ------------------- */
-// extern I2C_HandleTypeDef hi2c1;
-// extern UART_HandleTypeDef huart3;
+extern I2C_HandleTypeDef hi2c1;
+extern UART_HandleTypeDef huart3;
 
 // /* ------------------- State variables ------------------- */
-// static uint32_t rtd_press_start_time;
-// static uint32_t rtd_buzzer_start_time = 0;
-// static uint8_t set_rtd_buzzer = 1;
-// static uint8_t IO_state = 0xFF;
-// static uint8_t r2d = 0;
+static uint32_t rtd_press_start_time;
+static uint32_t rtd_buzzer_start_time = 0;
+static uint8_t set_rtd_buzzer = 1;
+static uint8_t IO_state = 0xFF;
+static uint8_t r2d = 0;
 
-// static FEB_SM_ST_t bms_state;
-// static FEB_SM_ST_t prev_state;
+static FEB_SM_ST_t bms_state;
+static FEB_SM_ST_t prev_state;
 
-// static uint8_t entered_drive = 0;
-// static uint8_t exited_drive = 0;
-// static uint32_t exit_buzzer_start_time = 0;
+static uint8_t entered_drive = 0;
+static uint8_t exited_drive = 0;
+static uint32_t exit_buzzer_start_time = 0;
 
-// static uint32_t datalog_press_start_time = 0;
-// static uint8_t datalog_active = 0;
+typedef struct
+{
+  // Switch states
+  bool coolant_pump_radiator_fan;
+  bool accumulator_fans;
 
-// static uint8_t tssi_startup = 0;
-// static uint8_t imd_startup = 0;
+  // Button states
+  bool ready_to_drive;
+
+  // State states
+  bool driving;
+  bool logging;
+  bool buzzer;
+} IO_State_t;
+
+IO_State_t state = {.coolant_pump_radiator_fan = 0,
+                    .accumulator_fans = 0,
+                    .ready_to_drive = 0,
+                    .driving = 0,
+                    .logging = 0,
+                    .buzzer = 0};
 
 // /* ------------------- Utilities ------------------- */
 // uint8_t set_n_bit(uint8_t x, uint8_t n, uint8_t bit_value)
@@ -46,221 +64,113 @@
 // }
 
 // /* ------------------- Initialization ------------------- */
-// void FEB_IO_Init(void)
-// {
-//   uint8_t init_val = 0xF;
-//   FEB_I2C_Master_Transmit(&hi2c1, IOEXP_ADDR << 1, &init_val, 1, HAL_MAX_DELAY);
-//   bms_state = FEB_CAN_BMS_Get_State();
-// }
+void FEB_IO_Init(void)
+{
+  uint8_t init_val = 0xF;
+  FEB_I2C_Master_Transmit(&hi2c1, IOEXP_ADDR << 1, &init_val, 1, HAL_MAX_DELAY);
+  // bms_state = FEB_CAN_BMS_Get_State();
+}
 
 // /* ------------------- Reset ------------------- */
-// void FEB_IO_Reset_All(void)
-// {
-//   rtd_press_start_time = 0;
-//   rtd_buzzer_start_time = 0;
-//   set_rtd_buzzer = 1;
-//   r2d = 0;
+void FEB_IO_Reset_All(void)
+{
+  rtd_press_start_time = 0;
+  rtd_buzzer_start_time = 0;
+  set_rtd_buzzer = 1;
+  r2d = 0;
 
-//   entered_drive = 0;
-//   exited_drive = 0;
-//   exit_buzzer_start_time = 0;
+  entered_drive = 0;
+  exited_drive = 0;
+  exit_buzzer_start_time = 0;
+}
 
-//   datalog_press_start_time = 0;
-//   datalog_active = 0;
-// }
+// /* ------------------- Ready To Drive Button ------------------- */
+void FEB_IO_HandleRTDButton(void)
+{
+  uint8_t received_data = 0x00;
+  FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
 
-// /* ------------------- TSSI & IMD ------------------- */
-// void FEB_IO_HandleTSSI(void)
-// {
-//   if (FEB_CAN_BMS_GET_FAULTS())
-//   {
-//     if (tssi_startup)
-//       HAL_GPIO_WritePin(GPIOG, GPIO_PIN_10, GPIO_PIN_RESET);
-//     else
-//       HAL_GPIO_WritePin(GPIOG, GPIO_PIN_10, GPIO_PIN_SET);
-//   }
-//   else
-//   {
-//     tssi_startup = 1;
-//     HAL_GPIO_WritePin(GPIOG, GPIO_PIN_10, GPIO_PIN_SET);
-//   }
-// }
+  uint8_t brake_pressure = FEB_CAN_PCU_GetLastBreakPosition();
+  uint8_t inv_enabled = FEB_CAN_PCU_GetLastRMSEnabled();
 
-// void FEB_IO_HandleIMD(void)
-// {
-//   if (FEB_CAN_GET_IMD_FAULT())
-//   {
-//     if (imd_startup)
-//       HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_SET);
-//     else
-//       HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_RESET);
-//   }
-//   else
-//   {
-//     imd_startup = 1;
-//     HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, GPIO_PIN_RESET);
-//   }
-// }
+  prev_state = bms_state;
+  bms_state = FEB_CAN_BMS_Get_State();
 
-// /* ------------------- RTD Button ------------------- */
-// void FEB_IO_HandleRTDButton(void)
-// {
-//   uint8_t received_data = 0x00;
-//   FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
+  /* Reset all if LV */
+  if (bms_state == FEB_SM_ST_LV)
+    FEB_IO_Reset_All();
 
-//   uint8_t brake_pressure = FEB_CAN_APPS_Get_Brake_Pos();
-//   uint8_t inv_enabled = FEB_CAN_APPS_Get_Enabled();
+  /* Detect transitions for buzzer */
+  if (prev_state == FEB_SM_ST_ENERGIZED && bms_state == FEB_SM_ST_DRIVE)
+    entered_drive = 1;
+  else if (prev_state == FEB_SM_ST_DRIVE && bms_state == FEB_SM_ST_ENERGIZED)
+    exited_drive = 1;
 
-//   prev_state = bms_state;
-//   bms_state = FEB_CAN_BMS_Get_State();
+  /* UI colors */
+  if (bms_state == FEB_SM_ST_DRIVE)
+  {
+    if (inv_enabled)
+      lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0x019F02), LV_PART_MAIN);
+    else
+      lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFFFF00), LV_PART_MAIN);
+  }
+  else if (bms_state == FEB_SM_ST_ENERGIZED)
+  {
+    lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFFFF00), LV_PART_MAIN);
+  }
+  else
+  {
+    lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFE0000), LV_PART_MAIN);
+    r2d = 0;
+  }
 
-//   /* Reset all if LV */
-//   if (bms_state == FEB_SM_ST_LV)
-//     FEB_IO_Reset_All();
+  /* RTD button logic */
+  if (received_data & (1 << 1))
+  {
+    if ((HAL_GetTick() - rtd_press_start_time) >= BTN_HOLD_TIME && brake_pressure >= 20 &&
+        (bms_state == FEB_SM_ST_ENERGIZED || bms_state == FEB_SM_ST_DRIVE))
+    {
+      // Attempt to enter or exit drive
+      if (bms_state == FEB_SM_ST_ENERGIZED)
+      {
+        r2d = 1; // Try entering Drive
+      }
+      else if (bms_state == FEB_SM_ST_DRIVE)
+      {
+        r2d = 0; // Try exiting Drive
+      }
 
-//   /* Detect transitions for buzzer */
-//   if (prev_state == FEB_SM_ST_ENERGIZED && bms_state == FEB_SM_ST_DRIVE)
-//     entered_drive = 1;
-//   else if (prev_state == FEB_SM_ST_DRIVE && bms_state == FEB_SM_ST_ENERGIZED)
-//     exited_drive = 1;
-
-//   /* UI colors */
-//   if (bms_state == FEB_SM_ST_DRIVE)
-//   {
-//     if (inv_enabled)
-//       lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0x019F02), LV_PART_MAIN);
-//     else
-//       lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFFFF00), LV_PART_MAIN);
-//   }
-//   else if (bms_state == FEB_SM_ST_ENERGIZED)
-//   {
-//     lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFFFF00), LV_PART_MAIN);
-//   }
-//   else
-//   {
-//     lv_obj_set_style_bg_color(ui_ButtonRTD, lv_color_hex(0xFE0000), LV_PART_MAIN);
-//     r2d = 0;
-//   }
-
-//   /* RTD button logic */
-//   if (received_data & (1 << 1))
-//   {
-//     if ((HAL_GetTick() - rtd_press_start_time) >= BTN_HOLD_TIME && brake_pressure >= 20 &&
-//         (bms_state == FEB_SM_ST_ENERGIZED || bms_state == FEB_SM_ST_DRIVE))
-//     {
-//       // Attempt to enter or exit drive
-//       if (bms_state == FEB_SM_ST_ENERGIZED)
-//       {
-//         r2d = 1; // Try entering Drive
-//       }
-//       else if (bms_state == FEB_SM_ST_DRIVE)
-//       {
-//         r2d = 0; // Try exiting Drive
-//       }
-
-//       // Send R2D over CAN
-//       IO_state = (uint8_t)set_n_bit(IO_state, 1, r2d);
-//       FEB_CAN_ICS_Transmit_Button_State(IO_state);
-//       rtd_press_start_time = HAL_GetTick(); // reset timer
-//     }
-//     else
-//     {
-//       IO_state = set_n_bit(IO_state, 1, r2d);
-//     }
-//   }
-//   else
-//   {
-//     rtd_press_start_time = HAL_GetTick();
-//     IO_state = set_n_bit(IO_state, 1, r2d);
-//   }
-// }
-
-// /* ------------------- Data Logger Button ------------------- */
-// void FEB_IO_HandleDataLoggerButton(void)
-// {
-//   uint8_t received_data = 0x00;
-//   FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
-
-//   if (received_data & (1 << 2))
-//   {
-//     if ((HAL_GetTick() - datalog_press_start_time) >= BTN_HOLD_TIME)
-//     {
-//       datalog_active = !datalog_active;
-//       IO_state = set_n_bit(IO_state, 2, datalog_active);
-//       datalog_press_start_time = HAL_GetTick();
-//     }
-//     else
-//     {
-//       IO_state = set_n_bit(IO_state, 2, datalog_active);
-//     }
-//   }
-//   else
-//   {
-//     datalog_press_start_time = HAL_GetTick();
-//     IO_state = set_n_bit(IO_state, 2, datalog_active);
-//   }
-
-//   /* UI feedback */
-//   if (datalog_active)
-//     lv_obj_set_style_bg_color(ui_ButtonDataLog, lv_color_hex(0x019F02), LV_PART_MAIN);
-//   else
-//     lv_obj_set_style_bg_color(ui_ButtonDataLog, lv_color_hex(0xFE0000), LV_PART_MAIN);
-// }
+      // Send R2D over CAN
+      IO_state = (uint8_t)set_n_bit(IO_state, 1, r2d);
+      FEB_CAN_ICS_Transmit_Button_State(IO_state);
+      rtd_press_start_time = HAL_GetTick(); // reset timer
+    }
+    else
+    {
+      IO_state = set_n_bit(IO_state, 1, r2d);
+    }
+  }
+  else
+  {
+    rtd_press_start_time = HAL_GetTick();
+    IO_state = set_n_bit(IO_state, 1, r2d);
+  }
+}
 
 // /* ------------------- Switches ------------------- */
-// /* ------------------- Coolant Pump Switch ------------------- */
-// void FEB_IO_HandleSwitch_CoolantPump(void)
-// {
-//   uint8_t received_data = 0x00;
-//   FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
+void FEB_IO_Handle_Switches(void)
+{
+  uint8_t received_data = 0x00;
+  FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
 
-//   if (received_data & (1 << 5))
-//   {
-//     IO_state = set_n_bit(IO_state, 5, 1);
-//     lv_obj_set_style_bg_color(ui_ButtonCoolPump, lv_color_hex(0x019F02), LV_PART_MAIN);
-//   }
-//   else
-//   {
-//     IO_state = set_n_bit(IO_state, 5, 0);
-//     lv_obj_set_style_bg_color(ui_ButtonCoolPump, lv_color_hex(0xFE0000), LV_PART_MAIN);
-//   }
-// }
+  //   00010010 received_data
+  //   00010000 (1 << 5)
+  // & 00010000 -> (bool) -> true
 
-// /* ------------------- Radiator Fan Switch ------------------- */
-// void FEB_IO_HandleSwitch_RadiatorFan(void)
-// {
-//   uint8_t received_data = 0x00;
-//   FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
-
-//   if (received_data & (1 << 6))
-//   {
-//     IO_state = set_n_bit(IO_state, 6, 1);
-//     lv_obj_set_style_bg_color(ui_ButtonRADFan, lv_color_hex(0x019F02), LV_PART_MAIN);
-//   }
-//   else
-//   {
-//     IO_state = set_n_bit(IO_state, 6, 0);
-//     lv_obj_set_style_bg_color(ui_ButtonRADFan, lv_color_hex(0xFE0000), LV_PART_MAIN);
-//   }
-// }
-
-// /* ------------------- Accumulator Fan Switch ------------------- */
-// void FEB_IO_HandleSwitch_AccumulatorFan(void)
-// {
-//   uint8_t received_data = 0x00;
-//   FEB_I2C_Master_Receive(&hi2c1, IOEXP_ADDR << 1, &received_data, 1, HAL_MAX_DELAY);
-
-//   if (received_data & (1 << 7))
-//   {
-//     IO_state = set_n_bit(IO_state, 7, 1);
-//     lv_obj_set_style_bg_color(ui_ButtonAccFan, lv_color_hex(0x019F02), LV_PART_MAIN);
-//   }
-//   else
-//   {
-//     IO_state = set_n_bit(IO_state, 7, 0);
-//     lv_obj_set_style_bg_color(ui_ButtonAccFan, lv_color_hex(0xFE0000), LV_PART_MAIN);
-//   }
-// }
+  state.logging = (bool)(received_data & (1 << 6));
+  state.coolant_pump_radiator_fan = (bool)(received_data & (1 << 5));
+  state.accumulator_fans = (bool)(received_data & (1 << 7));
+}
 
 // /* ------------------- Buzzer ------------------- */
 // void FEB_IO_HandleBuzzer(void)
--- DASH/Core/User/Src/FEB_UI_Helpers.c
+++ DASH/Core/User/Src/FEB_UI_Helpers.c
@@ -4,37 +4,55 @@
  */
 
 #include "FEB_UI_Helpers.h"
-#include "FEB_CAN_RMS.h"
+#include "FEB_CAN_PCU.h"
 #include "lvgl.h"
+#include "src/core/lv_obj_pos.h"
+#include "src/core/lv_obj_style.h"
+#include "src/draw/lv_draw_rect.h"
+#include "src/misc/lv_area.h"
 #include <stdio.h>
+#include <string.h>
+
+#define UI_DOT_COUNT 21
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 480
+#define MAX_TORQUE 300
 
 // ── UI objects ────────────────────────────────────────────────────────
 lv_obj_t *ui_Screen1;
 
-static lv_obj_t *ui_Title;       // "FORMULA ELECTRIC" header
-static lv_obj_t *ui_Berkeley;    // "BERKELEY" subheader
-static lv_obj_t *ui_TorqueLabel; // "TORQUE" caption
-static lv_obj_t *ui_TorqueValue; // live torque number
-static lv_obj_t *ui_TorqueUnit;  // "Nm" unit
-static lv_obj_t *ui_Divider;     // horizontal rule
+static lv_obj_t *ui_Title;                       // "FORMULA ELECTRIC" header
+static lv_obj_t *ui_Berkeley;                    // "BERKELEY" subheader
+static lv_obj_t *ui_TorqueLabel;                 // "TORQUE" caption
+static lv_obj_t *ui_TorqueValue;                 // live torque number
+static lv_obj_t *ui_TorqueUnit;                  // "Nm" unit
+static lv_obj_t *ui_Divider;                     // horizontal rule
+static lv_obj_t *ui_TorqueCircles[UI_DOT_COUNT]; // torque dots
 
 void ui_set_torque(int16_t torque)
 {
-  char buf[16];
-  snprintf(buf, sizeof(buf), "%d", torque);
-  lv_label_set_text(ui_TorqueValue, buf);
+  // char buf[16];
+  // snprintf(buf, sizeof(buf), "%d", torque);
+  // lv_label_set_text(ui_TorqueValue, buf);
 
-  if (torque > 0)
+  for (int i = 0; i < UI_DOT_COUNT; i++)
   {
-    lv_obj_set_style_text_color(ui_TorqueValue, lv_color_hex(0x00FF88), 0);
-  }
-  else if (torque < 0)
-  {
-    lv_obj_set_style_text_color(ui_TorqueValue, lv_color_hex(0xFF4444), 0);
-  }
-  else
-  {
-    lv_obj_set_style_text_color(ui_TorqueValue, lv_color_hex(0xAAAAAA), 0);
+    bool filled = i <= (150 * 21 / MAX_TORQUE);
+    if (i < 12)
+    {
+      lv_obj_set_style_bg_color(ui_TorqueCircles[i], filled ? lv_color_hex(0x00FF00) : lv_color_hex(0x004D00),
+                                0); // green
+    }
+    else if (i < 18)
+    {
+      lv_obj_set_style_bg_color(ui_TorqueCircles[i], filled ? lv_color_hex(0xFFDB00) : lv_color_hex(0x4D4900),
+                                0); // yellow
+    }
+    else
+    {
+      lv_obj_set_style_bg_color(ui_TorqueCircles[i], filled ? lv_color_hex(0xFF0000) : lv_color_hex(0x4D0000),
+                                0); // red
+    }
   }
 }
 
@@ -43,8 +61,18 @@ void ui_init(void)
 {
   // ── Screen ───────────────────────────────────────────────────────
   ui_Screen1 = lv_obj_create(NULL);
-  lv_obj_set_style_bg_color(ui_Screen1, lv_color_hex(0x000D1A), 0); // near-black deep blue
+  lv_obj_set_style_bg_color(ui_Screen1, lv_color_hex(0x000000), 0); // near-black deep blue
   lv_obj_set_style_bg_opa(ui_Screen1, LV_OPA_COVER, 0);
+
+  for (int i = 0; i < UI_DOT_COUNT; i++)
+  {
+    ui_TorqueCircles[i] = lv_obj_create(ui_Screen1);
+    lv_obj_align(ui_TorqueCircles[i], LV_ALIGN_TOP_LEFT, (i * (SCREEN_WIDTH - 20)) / UI_DOT_COUNT + 20, 15);
+    lv_obj_set_style_radius(ui_TorqueCircles[i], LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(ui_TorqueCircles[i], 23, 23);
+    lv_obj_set_style_border_width(ui_TorqueCircles[i], 0, 0);
+    lv_obj_set_style_bg_color(ui_TorqueCircles[i], lv_color_hex(0x00FF00), 0);
+  }
 
   // ── "FORMULA ELECTRIC" title ──────────────────────────────────────
   ui_Title = lv_label_create(ui_Screen1);
@@ -95,12 +123,7 @@ void ui_init(void)
 // ── ui_update ─────────────────────────────────────────────────────────
 void ui_update(void)
 {
-  static int16_t fake_torque = 0;
-  fake_torque += 5;
-  if (fake_torque > 300)
-    fake_torque = -100;
-
-  ui_set_torque(fake_torque);
+  ui_set_torque(FEB_CAN_PCU_GetLastTorque());
   lv_timer_handler();
 }
 
@@ -117,5 +140,10 @@ void ui_destroy(void)
     ui_TorqueValue = NULL;
     ui_TorqueUnit = NULL;
     ui_Divider = NULL;
+
+    for (int i = 0; i < UI_DOT_COUNT; i++)
+    {
+      ui_TorqueCircles[i] = NULL;
+    }
   }
 }
