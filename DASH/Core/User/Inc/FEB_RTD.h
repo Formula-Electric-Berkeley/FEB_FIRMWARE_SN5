a11201bfe45c61895ac6efebceba518387677554
asymptonic <86031051+asymptonic@users.noreply.github.com>
2026-03-29 21:33:52 -0700

    more progress on rtd code

--- DASH/Core/User/Inc/FEB_RTD.h
+++ DASH/Core/User/Inc/FEB_RTD.h
@@ -13,6 +13,13 @@
 #define BUZZER_DURATION_RTD_ENTER 2000
 #define BUZZER_DURATION_RTD_EXIT 500
 
+typedef enum
+{
+  NOT_BUZZED = 0,
+  BUZZED_ENTER_RTD,
+  BUZZED_EXIT_RTD
+} Buzzing_State_t;
+
 void FEB_State_Update_RTD(void);
 
 #endif /* FEB_RTD_H */
--- DASH/Core/User/Src/FEB_RTD.c
+++ DASH/Core/User/Src/FEB_RTD.c
@@ -9,23 +9,32 @@
 static uint32_t rtd_button_press_start_tick = 0;
 static bool previous_rtd_button = false;
 
+static Buzzing_State_t buzzing_state =
+    NOT_BUZZED; // Here to ensure FEB_IO_Play_Buzzer() is not spammed and only called once
+
 void FEB_State_Update_RTD(void)
 {
-  // uint8_t brake_pressure = FEB_CAN_PCU_GetLastBreakPosition();
-  // uint8_t inv_enabled = FEB_CAN_PCU_GetLastRMSEnabled();
+  // MARK: Start buzzer code
+  // to react to BMS entering and exiting drive state
   BMS_State_t bms_state = FEB_CAN_BMS_GetLastState();
   BMS_State_t previous_bms_state = FEB_CAN_BMS_GetLastState();
-
-  IO_Switch_States_t states = FEB_IO_GetLastIOStates();
-
-  if (previous_bms_state == BMS_STATE_ENERGIZED && bms_state == BMS_STATE_DRIVE)
+  if (previous_bms_state == BMS_STATE_ENERGIZED && bms_state == BMS_STATE_DRIVE && buzzing_state != BUZZED_ENTER_RTD)
   {
     FEB_IO_Play_Buzzer(BUZZER_DURATION_RTD_ENTER);
+    buzzing_state = BUZZED_ENTER_RTD;
   }
-  else if (previous_bms_state == BMS_STATE_DRIVE && bms_state == BMS_STATE_ENERGIZED)
+  else if (previous_bms_state == BMS_STATE_DRIVE && bms_state == BMS_STATE_ENERGIZED &&
+           buzzing_state != BUZZED_EXIT_RTD)
   {
     FEB_IO_Play_Buzzer(BUZZER_DURATION_RTD_EXIT);
+    buzzing_state = BUZZED_EXIT_RTD;
   }
+
+  // MARK: Signal enter rtd code
+  // Send the ready to drive message over CAN when all the conditions are met
+  IO_Switch_States_t states = FEB_IO_GetLastIOStates();
+  uint8_t brake_pressure = FEB_CAN_PCU_GetLastBreakPosition();
+  uint8_t inv_enabled = FEB_CAN_PCU_GetLastRMSEnabled();
 
   if (previous_rtd_button == false && states.button_rtd)
   {
