b4beda117c4fe716e200911aa223e7d84c59881c
hetvi15986 <hetvi@berkeley.edu>
2026-04-09 14:34:34 -0700

    fixed can_id in FEB_CAN_WSS.c

--- Sensor_Nodes/Core/User/Src/FEB_CAN_WSS.c
+++ Sensor_Nodes/Core/User/Src/FEB_CAN_WSS.c
@@ -37,13 +37,8 @@ void FEB_CAN_WSS_Tick(void)
   memcpy(&tx_data[0], &left_rpm, sizeof(uint8_t));
   memcpy(&tx_data[1], &right_rpm, sizeof(uint8_t));
 
-  FEB_CAN_Status_t status =
-      FEB_CAN_TX_Send(FEB_CAN_INSTANCE_1, FEB_CAN_WSS_DATA_FRAME_ID, FEB_CAN_ID_STD, tx_data, FEB_CAN_WSS_DATA_LENGTH);
-  if (status != FEB_CAN_OK)
-  {
-    can_tx_error_count++;
-  }
-
+  FEB_CAN_Status_t status = FEB_CAN_TX_Send(FEB_CAN_INSTANCE_1, FEB_CAN_WSS_FRONT_DATA_FRAME_ID, FEB_CAN_ID_STD,
+                                            tx_data, FEB_CAN_WSS_FRONT_DATA_LENGTH);
   if (status != FEB_CAN_OK)
   {
     can_tx_error_count++;
